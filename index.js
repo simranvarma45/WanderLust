06308fad9ff6cd7b70309db5899a12b56bc8d004
simran verma <vermasimran514@gmail.com>
2025-07-18 17:32:29 +0530

    changes routes

--- index.js
+++ index.js
@@ -90,8 +90,8 @@ app.use((req,res,next)=>{
 
 app.use("/listing", listingRouter);
 app.use("/listing/:id/reviews", reviewRouter);
-// app.use("/",userRouter);
-app.use("/",listingRouter);
+app.use("/",userRouter);
+
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!!"));
